42f3d3dc538fe825b8b77685639092761ce1d008
dantegg <luroufan327@hotmail.com>
2016-09-09 22:54:11 +0800

    fix bugs

--- simple_counter/index.js
+++ simple_counter/index.js
@@ -2,15 +2,15 @@
  * Created by dantegg on 16/9/8.
  */
 import React from 'react'
-import {render} from 'react-dom'
-import {Provider} from 'react-redux'
-import  configureStore from './store/configureStore'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
 import App from './containers/App'
+import  configureStore from './store/configureStore'
 const store = configureStore()
 
 render(
-    <Provider store={store}>
+    <Provider store={store} >
         <App />
-        </Provider>,
+    </Provider>,
     document.getElementById('root')
 )
